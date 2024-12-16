3a83378a2586a466a76d62c0fe87d376341f1204
Sreeteja332 <sreeteja332@gmail.com>
2024-12-16 18:24:53 +0530

    10.7

--- reviews/views.py
+++ reviews/views.py
@@ -7,10 +7,16 @@ from django.shortcuts import render
 def review(request):
     if request.method=='POST':
         entered_username=request.POST['username']
+        if entered_username=="":
+            return render(request,"reviews/reviews.html",{
+                "has_error":True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
     
-    return render(request,"reviews/reviews.html")
+    return render(request,"reviews/reviews.html",{
+                "has_error":False
+        })
 
 
 def thank_you(request):
